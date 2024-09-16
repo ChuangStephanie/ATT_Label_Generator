e37bb7756054c334a10fa5cf1ee900feb31221c4
ChuangStephanie <stephanie231951@yahoo.com>
2024-09-16 16:15:27 -0500

    header barcodes now generate the correct size

--- src/render.js
+++ src/render.js
@@ -98,28 +98,80 @@ function updateLabels() {
     const skuLabel = document.querySelector(".skuNum");
     skuLabel.firstChild.textContent = `AT&T SKU: ${sku}`;
     const skuContainer = barcodeElements.sku;
-    skuContainer.innerHTML = sku;
+    skuContainer.innerHTML = "";
+    const barcodeSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+
+    skuContainer.appendChild(barcodeSvg);
+
+    JsBarcode(barcodeSvg, sku, {
+      format: "CODE128",
+      displayValue: false,
+      width: 2,
+      height: skuContainer.clientHeight,
+    });
   }
 
   if (orderNum) {
     const orderNumLabel = document.querySelector(".order");
     orderNumLabel.firstChild.textContent = `ORDER #: ${orderNum}`;
     const orderNumContainer = barcodeElements.orderNum;
-    orderNumContainer.innerHTML = orderNum;
+    orderNumContainer.innerHTML = "";
+    const barcodeSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+
+    orderNumContainer.appendChild(barcodeSvg);
+
+    JsBarcode(barcodeSvg, orderNum, {
+      format: "CODE128",
+      displayValue: false,
+      width: 2,
+      height: orderNumContainer.clientHeight,
+    });
   }
 
   if (qty) {
     const qtyLabel = document.querySelector(".carton");
     qtyLabel.firstChild.textContent = `CARTON QTY: ${qty}`;
     const qtyContainer = barcodeElements.qty;
-    qtyContainer.innerHTML = qty;
+    qtyContainer.innerHTML = "";
+    const barcodeSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+
+    qtyContainer.appendChild(barcodeSvg);
+
+    JsBarcode(barcodeSvg, qty, {
+      format: "CODE128",
+      displayValue: false,
+      width: 2,
+      height: 24,
+    });
   }
 
   if (caseId) {
     const caseIdLabel = document.querySelector(".case");
     caseIdLabel.firstChild.textContent = `CASE ID: ${caseId}`;
     const caseIdContainer = barcodeElements.caseId;
-    caseIdContainer.innerHTML = caseId;
+    caseIdContainer.innerHTML = "";
+    const barcodeSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+
+    caseIdContainer.appendChild(barcodeSvg);
+
+    JsBarcode(barcodeSvg, caseId, {
+      format: "CODE128",
+      displayValue: false,
+      width: 2,
+      height: 75
+    });
   }
 }
 
@@ -128,28 +180,35 @@ function hideHeaderInputs() {
     "#sku-input, #orderNum-input, #qty-input, #caseId-input, #device-model, #sw-version"
   );
   headerInputs.forEach((input) => {
-    input.style.display = "hidden";
+    input.style.display = "none";
   });
 }
 
 function generatePdf() {
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF({ orientation: 'landscape', unit: 'px', format: [1056, 816] });
-  
-    document.querySelectorAll('#sku-input, #orderNum-input, #qty-input, #caseId-input, #device-model, #sw-version')
-    .forEach(input => input.classList.add('pdf-hide'));
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF({
+    orientation: "landscape",
+    unit: "px",
+    format: [1056, 816],
+  });
 
-    // Target the main container to convert it to PDF
-    const mainContainer = document.querySelector(".main-container");
-  
-    html2canvas(mainContainer, { scale: 2 }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const imgWidth = 1056; // PDF width in px
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      
-      // Add image to PDF
-      doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-      
-      doc.save('labels.pdf');
-    });
-  }
+  document
+    .querySelectorAll(
+      "#sku-input, #orderNum-input, #qty-input, #caseId-input, #device-model, #sw-version"
+    )
+    .forEach((input) => input.classList.add("pdf-hide"));
+
+  // Target the main container to convert it to PDF
+  const mainContainer = document.querySelector(".main-container");
+
+  html2canvas(mainContainer, { scale: 2 }).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const imgWidth = 1056; // PDF width in px
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    // Add image to PDF
+    doc.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+
+    doc.save("labels.pdf");
+  });
+}
