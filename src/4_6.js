a4fd0f329475e157deb3b233c330c0d063d1295c
ChuangStephanie <stephanie231951@yahoo.com>
2024-09-16 17:30:47 -0500

    added homepage and started creating 4x6 label

---
+++ src/4_6.js
@@ -0,0 +1,176 @@
+const form = document.getElementById("imei-form");
+form.classList.add("imei-form-container");
+
+for (let i = 0; i < 10; i++) {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.name = "imei[]";
+    input.classList.add("imei-input");
+    input.placeholder = `IMEI ${i + 1}`;
+    form.appendChild(input);
+  }
+
+  function submitImeiNums() {
+    const imeiInputs = document.querySelectorAll('input[name="imei[]"]');
+    const imeiNums = [];
+    let isValid = true;
+    let errorMessage = "";
+  
+    imeiInputs.forEach((input) => {
+      const value = input.value.trim();
+  
+      if (value.length === 15) {
+        imeiNums.push(value);
+      }
+    });
+  
+    if (imeiNums.length === 0) {
+      errorMessage = "At least one valid IMEI number is required.";
+      isValid = false;
+    }
+  
+    if (!isValid) {
+      document.getElementById("error").textContent = errorMessage;
+      return;
+    }
+  
+    document.getElementById("error").textContent = "";
+    generateBarcodes(imeiNums);
+  
+    hideHeaderInputs();
+  }
+
+  function generateBarcodes(imeiNums) {
+    const imeiContainer = document.querySelector(".IMEI");
+    imeiContainer.innerHTML = "";
+  
+    imeiNums.forEach((imei) => {
+      const imeiDiv = document.createElement("div");
+      imeiDiv.classList.add("imei-container");
+  
+      const barcodeSvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+  
+      imeiDiv.appendChild(barcodeSvg);
+  
+      imeiContainer.appendChild(imeiDiv);
+  
+      JsBarcode(barcodeSvg, imei, {
+        format: "CODE128",
+        displayValue: true,
+        width: 2,
+        height: 24,
+      });
+    });
+  
+    updateLabels();
+    generatePdf();
+  }
+
+  function updateLabels() {
+    const sku = document.getElementById("sku-input").value.trim();
+    const orderNum = document.getElementById("orderNum-input").value.trim();
+    const qty = document.getElementById("qty-input").value.trim();
+    const caseId = document.getElementById("caseId-input").value.trim();
+    const description = document.getElementById("description").value.trim();
+    const swVersion = document.getElementById("sw-version").value.trim();
+  
+    const barcodeElements = {
+      sku: document.getElementById("sku"),
+      orderNum: document.getElementById("orderNum"),
+      qty: document.getElementById("qty"),
+      caseId: document.getElementById("caseId"),
+    };
+  
+    if (description) {
+      const descriptionHeader = document.querySelector(".description");
+      descriptionHeader.firstChild.textContent = `Description: ${description}`;
+    }
+  
+    if (swVersion) {
+      const swVersionHeader = document.querySelector(".software");
+      swVersionHeader.firstChild.textContent = `SW VERSION: ${swVersion}`;
+    }
+  
+    if (sku) {
+      const skuLabel = document.querySelector(".skuNum");
+      skuLabel.firstChild.textContent = `AT&T SKU: ${sku}`;
+      const skuContainer = barcodeElements.sku;
+      skuContainer.innerHTML = "";
+      const barcodeSvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+  
+      skuContainer.appendChild(barcodeSvg);
+  
+      JsBarcode(barcodeSvg, sku, {
+        format: "CODE128",
+        displayValue: false,
+        width: 2,
+        height: skuContainer.clientHeight,
+      });
+    }
+  
+    if (orderNum) {
+      const orderNumLabel = document.querySelector(".order");
+      orderNumLabel.firstChild.textContent = `ORDER #: ${orderNum}`;
+      const orderNumContainer = barcodeElements.orderNum;
+      orderNumContainer.innerHTML = "";
+      const barcodeSvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+  
+      orderNumContainer.appendChild(barcodeSvg);
+  
+      JsBarcode(barcodeSvg, orderNum, {
+        format: "CODE128",
+        displayValue: false,
+        width: 2,
+        height: orderNumContainer.clientHeight,
+      });
+    }
+  
+    if (qty) {
+      const qtyLabel = document.querySelector(".carton");
+      qtyLabel.firstChild.textContent = `CARTON QTY: ${qty}`;
+      const qtyContainer = barcodeElements.qty;
+      qtyContainer.innerHTML = "";
+      const barcodeSvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+  
+      qtyContainer.appendChild(barcodeSvg);
+  
+      JsBarcode(barcodeSvg, qty, {
+        format: "CODE128",
+        displayValue: false,
+        width: 2,
+        height: 24,
+      });
+    }
+  
+    if (caseId) {
+      const caseIdLabel = document.querySelector(".case");
+      caseIdLabel.firstChild.textContent = `CASE ID: ${caseId}`;
+      const caseIdContainer = barcodeElements.caseId;
+      caseIdContainer.innerHTML = "";
+      const barcodeSvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+  
+      caseIdContainer.appendChild(barcodeSvg);
+  
+      JsBarcode(barcodeSvg, caseId, {
+        format: "CODE128",
+        displayValue: false,
+        width: 2,
+        height: 75
+      });
+    }
+  }
